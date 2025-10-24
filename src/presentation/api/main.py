9e933a774ba936e832ad7c03dad51bda1140778d
andreievDanylo <andreiev.danylo@airslate.com>
2025-10-24 11:12:44 +0200

    init

--- src/presentation/api/main.py
+++ src/presentation/api/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from src.config.settings import settings
 
 app = FastAPI(
-    title="Reviews Insights API"
+    title="Reviews Insights API",
+    debug=settings.debug
 )
 
 
 @app.get("/")
 async def root():
     return {
-        "message": "Reviews"
+        "message": "Reviews Insights API",
+        "debug": settings.debug,
+        "openai_model": settings.openai_model
     }
